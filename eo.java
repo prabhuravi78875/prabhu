b28f0e7ff580bfbc7a95a70b9fff4a44e88a7124
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-07-21 09:33:10 +0530

    Update eo.java

--- eo.java
+++ eo.java
@@ -3,6 +3,7 @@ public class guvi {
 public static void main(String[] args){
 	Scanner in=new Scanner(System.in);
 	int x=in.nextInt();
+	if(x>0){
 	if(x%2==0)
 	{
 		System.out.println("even");
@@ -10,7 +11,12 @@ public static void main(String[] args){
 	else
 	{
 		System.out.println("odd");
+	}}
+	else
+	{
+		System.out.println("Invalid");
 	}
 	in.close();
 }
 }
+
