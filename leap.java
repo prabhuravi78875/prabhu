a26a8461ba61c9d2238a1ddce3486079896025b9
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-07-21 10:23:21 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	if(a%4==0)
+	{
+		System.out.println("Yes");
+	}
+	else
+	{
+		System.out.println("No");
+	}
+	in.close();
+}
+}
