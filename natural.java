290d1da90ff225755411974c8121a2031af77027
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-07-21 10:41:46 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int sum=0;
+	for(int i=0;i<=a;i++)
+	{
+		sum=sum+i;
+	}
+	System.out.println(sum);
+	in.close();
+}
+}
