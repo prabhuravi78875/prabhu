4d1ada453586e933b4fb3efe15f2cf11ce104d0b
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-07-21 09:30:57 +0530

    Create eo.java

---
+++ eo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int x=in.nextInt();
+	if(x%2==0)
+	{
+		System.out.println("even");
+	}
+	else
+	{
+		System.out.println("odd");
+	}
+	in.close();
+}
+}
