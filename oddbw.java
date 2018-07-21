d96180cb878097364c744ce09c2c3d3d72b5f4e7
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-07-21 12:21:45 +0530

    Create oddbw.java

---
+++ oddbw.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int b=in.nextInt();
+	for(int i=a+1;i<b;i++)
+	{
+		if(i%2!=0)
+		{
+			System.out.println(i);
+		}
+	}
+	in.close();
+}
+}
