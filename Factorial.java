393414c65fbfbdd398e4efeacc291ab3d84c743b
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-07-31 10:31:30 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+package prp;
+import java.util.*;
+public class guvi {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int fact=1;
+	for(int i=1;i<=a;i++)
+	{
+		fact=fact*i;
+		System.out.println(fact);
+	}
+	System.out.println(fact);
+	in.close();
+}
+}
