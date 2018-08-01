c28b4dd1791cf8298feb7a790ca2e02faa51f464
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-08-01 12:14:10 +0530

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class guvi {
+	public static void main(String[] arg)
+    {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		System.out.println(n/60+" "+n%60);
+		in.close();
+    }
+}
