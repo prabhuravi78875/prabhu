ea89548929e5c48a189b14ba33d742a696c78603
prabhuravi78875 <41105786+prabhuravi78875@users.noreply.github.com>
2018-08-06 11:25:57 +0530

    Create CountALetter.java

---
+++ CountALetter.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class guvi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String v,r;
+		Scanner in=new Scanner(System.in);
+		r=in.nextLine();
+		v=r.replaceAll(" ","");
+		int l=v.length();
+		System.out.println(l);
+		in.close();
+	}
+
+}
